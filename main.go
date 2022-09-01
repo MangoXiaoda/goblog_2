fe1d8202f2bc699878ba601117769cdbcd27110b
lizhongda <944872615@qq.com>
2022-09-01 10:59:14 +0800

    使用中间件

--- main.go
+++ main.go
@@ -37,6 +37,15 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1、设置标头
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// 2、继续处理请求
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	router := mux.NewRouter()
 
@@ -49,6 +58,9 @@ func main() {
 
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 中间件：强制内容类型为 HTML
+	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
