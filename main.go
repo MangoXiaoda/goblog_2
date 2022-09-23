d9661bc7165031a77616c9d47be69f21427b49c9
lizhongda <944872615@qq.com>
2022-09-23 16:39:24 +0800

    重构 GetRouteVariable

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func (a Article) Link() string {
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1、获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2、读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -87,11 +87,6 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
 func getArticleByID(id string) (Article, error) {
 	article := Article{}
 	query := "SELECT * FROM articles WHERE id = ?"
@@ -102,7 +97,7 @@ func getArticleByID(id string) (Article, error) {
 func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1、获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2、读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -139,7 +134,7 @@ func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1、获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2、读取对应的文章数据
 	_, err := getArticleByID(id)
@@ -442,7 +437,7 @@ func createTables() {
 func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 1、获取 URL 参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2、读取对应的文章数据
 	article, err := getArticleByID(id)
