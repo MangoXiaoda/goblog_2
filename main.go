cc7b8709f6b2fd4bebd6b8002535aa44dd8aadf1
lizhongda <944872615@qq.com>
2022-10-08 16:20:22 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,129 +1,16 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"goblog_2/app/http/middlewares"
 	"goblog_2/bootstrap"
-	"goblog_2/pkg/database"
+	"goblog_2/pkg/logger"
 	"net/http"
-	"net/url"
-	"strings"
-
-	"github.com/gorilla/mux"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog! </h1>")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+"<a href=\"http://www.baidu.com\">mango@example.com</a>")
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
-}
-
-// ArticlesFormData 创建博文表单数据
-type ArticlesFormData struct {
-	Title, Body string
-	URL         *url.URL
-	Errors      map[string]string
-}
-
-func SaveArticleToDB(title string, body string) (int64, error) {
-
-	// 变量初始化
-	var (
-		id   int64
-		err  error
-		rs   sql.Result
-		stmt *sql.Stmt
-	)
-
-	// 1、获取一个 prepare 声明语句
-	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
-	// 例行的错误检测
-	if err != nil {
-		return 0, err
-	}
-
-	// 2、在此函数运行结束后关闭此语句，防止占用 SQL 连接
-	defer stmt.Close()
-
-	// 3、执行请求，传参进入绑定的内容
-	rs, err = stmt.Exec(title, body)
-	if err != nil {
-		return 0, err
-	}
-
-	// 4、插入成功的话，会返回自增 ID
-	if id, err = rs.LastInsertId(); id > 0 {
-		return id, nil
-	}
-
-	return 0, err
-}
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1、设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2、继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// 1、除首页外，移除所有请求路径后面的斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
-// RouteName2URL 通过路由名称来获取 URL
-// func RouteName2URL(routeName string, pairs ...string) string {
-// 	url, err := router.Get(routeName).URL(pairs...)
-// 	if err != nil {
-// 		logger.LogError(err)
-// 		return ""
-// 	}
-
-// 	return url.String()
-// }
-
 func main() {
-
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 自定义 404 页面
-	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL :", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
