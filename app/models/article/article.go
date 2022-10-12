4483463e66f957665d0bae42ecde94afccb167ce
lizhongda <944872615@qq.com>
2022-10-12 15:03:26 +0800

    模型基类和 view 包

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -1,13 +1,15 @@
 package article
 
 import (
+	"goblog_2/app/models"
 	"goblog_2/pkg/route"
 	"strconv"
 )
 
 // Article 文章模型
 type Article struct {
-	ID    uint64
+	models.BaseModel
+
 	Title string
 	Body  string
 }
